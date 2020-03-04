0ab020389fe267d6f230346430bd6debdeed6424
devkirb <54935373+devkirb@users.noreply.github.com>
2020-03-04 09:36:32 +0100

    Update and rename 5_7.php to 5.7.php

--- 5.7.php
+++ 5.7.php
@@ -1,9 +1,8 @@
 <?php
 $kwadraat = 0;
 $input = 0;
-while($kwadraat <= 1000) {
-    $kwadraat = $input * $input;
-    $input++;
+    while($kwadraat <= 1000) {
+      $kwadraat = $input * $input;
+      $input++;
 }
 echo "Het antwoord is " . $kwadraat . PHP_EOL;
-,
